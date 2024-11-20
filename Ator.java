fd2c5512266c21363faa9a98352e139b123b8bca
Lucas Ximenes <lucasximenes177@gmail.com>
2024-11-20 12:09:28 -0300

    segundo

--- Ator.java
+++ Ator.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Ator extends Pessoa {
@@ -14,6 +17,16 @@ public class Ator extends Pessoa {
     public Ator(String cpf, String nome, String email, int registro) {
         super(cpf, nome, email);
         this.registro = registro;
+    }
+    public boolean cadastrar() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("atores.txt", true))) {
+            writer.write(this.registro + ";" + this.getNome());
+            writer.newLine();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     
