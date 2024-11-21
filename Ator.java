1f8203f4a854cacfec78d4daa992ed001e41463d
ALUNO T5 <alunot5@acad.unifor.br>
2024-11-21 19:35:41 -0300

    atualizando a classe ator

--- Ator.java
+++ Ator.java
@@ -19,8 +19,8 @@ public class Ator extends Pessoa {
         this.registro = registro;
     }
     public boolean cadastrar() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\alunot7\\Downloads\\SistemaCinema-main\\SistemaCinema-main\\BD\\atores.txt", true))) {
-            writer.write(this.registro + ";" + this.getNome());
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\alunot5\\Documents\\AV3\\SistemaCinema\\BD\\atores.txt", true))) {
+            writer.write(this.getNome() + ", " + this.getCpf() + ", " + getEmail() + ", " + this.registro + ".");
             writer.newLine();
             return true;
         } catch (IOException e) {
